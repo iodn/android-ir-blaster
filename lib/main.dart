6952e551abef7d6b1a8624e999d12166105771cb
Iodn <114117719+iodn@users.noreply.github.com>
2025-03-11 14:57:57 +0900

    Provide fallback color schemes in case dynamic color returns null

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,15 @@ void main() async {
 
   runApp(DynamicColorBuilder(
     builder: (lightDynamic, darkDynamic) {
+      final ColorScheme lightScheme =
+          lightDynamic ?? ColorScheme.fromSeed(seedColor: Colors.blue);
+      final ColorScheme darkScheme = darkDynamic ??
+          ColorScheme.fromSeed(
+              seedColor: Colors.blue, brightness: Brightness.dark);
       return MaterialApp(
         title: "IR Blaster",
-        theme: ThemeData(colorScheme: lightDynamic, useMaterial3: true),
-        darkTheme: ThemeData(colorScheme: darkDynamic, useMaterial3: true),
+        theme: ThemeData(colorScheme: lightScheme, useMaterial3: true),
+        darkTheme: ThemeData(colorScheme: darkScheme, useMaterial3: true),
         home: const RemoteList(),
       );
     },
